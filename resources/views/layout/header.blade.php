4127b86085b8145e9662af8b7eb191192da012f5
narendrabhattain <narendra_bhatta@live.com>
2021-08-29 19:34:38 +0545

    Initial Commit

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Mentor Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{asset('backend/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('backend/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('backend/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/aos/aos.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/backend/vender/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{asset('backend/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('backend/css/style.css')}}" rel="stylesheet">
+
+</head>
---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+    <title>
+    Site Main - Panel</title>
+    </head>
+    <body>
+            
+         
+            @include('layout.header')
+            @yield('content')
+            @include('layout.footer')
+    
+    </body>
+    </html>
